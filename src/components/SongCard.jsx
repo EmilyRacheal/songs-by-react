d5edf6affa2016db229ccd401bdc50577a916db2
emily <emilychealy4@gmail.com>
2022-10-13 15:57:37 +0100

    worked on dispalying the play pause button

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -3,15 +3,29 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ( {song, i} ) => {
-  const activeSong = 'Test';
+const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  // const activeSong = 'Test';/
+
+  const handlePauseClick = () => {
+
+  };
+
+  const handlePlayClick = () => {
+
+  }
 
   return(
       <div className="flex w-[250px] flex-col p-4 bg-white/5 bg-opacity-80 backdrop:backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
     
         <div className="relative w-full h-56 group">
           <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong.title === song.title ? "flex bg-black bg-opacity-70" :"hidden" }`}>
-            <playPause />
+            <PlayPause 
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+            />
           </div>
           <img src={song.images?.coverart} alt="song_img"/>
         </div>
