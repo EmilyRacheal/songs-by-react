176517101223b4079f57bcc8c6ddf77165f6d9af
emily <emilychealy4@gmail.com>
2022-10-13 14:08:12 +0100

    worked on the loader

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -6,8 +6,17 @@ import{createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
         reducerPath:'shazamCoreApi',
         baseQuery: fetchBaseQuery({
             baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
-            prepareHeaders: () => {
-                headers.set("X-RapidAPI-Key", "f277fecca7msh7fe2081ff13d105p189bcbjsn99349c015e7e" )
+            prepareHeaders: (headers) => {
+                headers.set("X-RapidAPI-Key", "f277fecca7msh7fe2081ff13d105p189bcbjsn99349c015e7e" );
+
+                return headers;
             },
         }),
+        endpoints:(builder) =>({
+            getTopCharts: builder.query({query: () => "/charts/world"}),
+        }),
     });
+
+    export const {
+        useGetTopChartsQuery,
+    } = shazamCoreApi
