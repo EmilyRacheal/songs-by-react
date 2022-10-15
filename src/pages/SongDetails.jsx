5588b1ef15e684f30e2bf4ee9d7ca8f406a53873
emily <emilychealy4@gmail.com>
2022-10-15 22:18:22 +0100

    working on the song detail component

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,22 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongSs } from "../components";
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { setActiveSong, isPlaying } = useSelector((state) => state.player);
+  console.log(songid);
+  return (
+    <div className="flex flex-col">
+      {/* <DetailsHeader artistsId={artistsId} songData={songData}/> */}
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+      </div>
+    </div>
+  );
+};
 
 export default SongDetails;
