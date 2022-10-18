3b1b811ae28ef51f524e0617b8deb0ace1afae0a
emily <emilychealy4@gmail.com>
2022-10-18 23:34:44 +0100

    working on the top charts

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,32 @@
-const TopCharts = () => <div>TopCharts</div>;
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { Error, Loader, SongCard } from "../components";
 
+const TopCharts = () => {
+
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading Top Charts" />
+  if (error) return <Error /> 
+
+  return (
+    <div className=" flex flex-col">
+        <h2 className="font-bold text-left text-3xl text-white mt-4 mb-10">Discover Top charts </h2>
+        <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+            {data?.map((song, i)=>(
+                <SongCard
+                key={song.key}
+                song={song}  
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                data={data}
+                i={i}
+            />
+        ))}
+        </div>
+  </div>);
+};
 export default TopCharts;
+
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -16,7 +16,9 @@ import{createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
             getTopCharts: builder.query({query: () => "/charts/world"}),
             getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
             getRelatedSongs: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
-            getArtistDetails: builder.query({ query: ({ artistId }) => `/tracks/related?track_id=${artistId}` }),
+            getArtistDetails: builder.query({ query: ({ artistId }) => `/artists/details?artist_id=${artistId}` }),
+            getSongsByCountry: builder.query({ query: ({ countryCode }) => `/charts/country?country_code=${countryCode}` }),
+
         }),
     });
 
@@ -25,4 +27,5 @@ import{createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
         useGetSongDetailsQuery,
         useGetRelatedSongsQuery,
         useGetArtistDetailsQuery,
+        useGetSongsByCountryQuery,
     } = shazamCoreApi
