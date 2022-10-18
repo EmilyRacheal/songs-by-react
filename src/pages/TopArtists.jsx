54ab2a715fe8e1260db3c5650ca0d9512f8a06fa
emily <emilychealy4@gmail.com>
2022-10-18 23:53:37 +0100

    working on the top artist display

--- src/pages/TopArtists.jsx
+++ src/pages/TopArtists.jsx
@@ -1,3 +1,23 @@
-const TopArtists = () => <div>TopArtists</div>;
+import axios from "axios";
+import { useGetTopChartsQuery } from "../redux/services/shazamCore";
+import { ArtistCard, Loader, Error } from "../components";
 
+const TopArtists = () => {
+  const { data, isFetching, error } = useGetTopChartsQuery();
+
+  if (isFetching) return <Loader title="Loading Top Charts" />
+  if (error) return <Error /> 
+
+  return (
+    <div className=" flex flex-col">
+        <h2 className="font-bold text-left text-3xl text-white mt-4 mb-10"> Top Artists</h2>
+        <div className="flex flex-wrap justify-center sm:justify-start gap-8">
+            {data?.map((track)=>(
+                <ArtistCard key={track.key} track={track}  />
+        ))}
+        </div>
+  </div>);
+};
 export default TopArtists;
+
+
